c52989712fc346733b84ff1326150062f2ae79ac
aihe <akhercha@student.42.fr>
2018-08-31 18:07:20 +0200

    added comments'

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const app = express();
 
 const SERVER_PORT = 8080;
 
+/* Set the view engine to EJS. */
 app.set("view engine", "ejs");
+/* Specify that static files are located in the public folder. */
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
 	res.render("index");
 });
 
+/* Redirect pages that do not exist to a 404 error. */
 app.use((req, res, next) => {
 	res.status(404).render("404");
 });
 
+/* Start the server. */
 app.listen(SERVER_PORT, () => {
 	console.log("Server for Random Game started on port " + SERVER_PORT + ".");
 });
